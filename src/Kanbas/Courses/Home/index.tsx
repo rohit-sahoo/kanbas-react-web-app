6e59dfe16769bdc64063734115c42ac0a897f7f6
rohitsahoo <rohitsahoo741@gmail.com>
2024-03-18 20:08:13 -0400

    a4 State and Redux fa23

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,7 +1,6 @@
 import ModuleList from "../Modules/moduleList";
 import Status from "./status";
 
-
 function Home() {
     return (
         <div className="container-fluid d-flex flex-row me-xxl-3">
